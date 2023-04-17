68d019266ceb622916d8ab5f1ee9ffd9cb597ac6
Gabriel Tollan <gabitol87@hotmail.com>
2023-04-17 00:26:51 -0300

    agregamos contenido

--- servidorExpress/src/routes/carts.router.js
+++ servidorExpress/src/routes/carts.router.js
@@ -1,21 +1,32 @@
 import { Router } from 'express';
+import CartsManager from "../../Managers/CartsManager.js";
 
 const router = Router();
+const manager = new CartsManager();
 
-const carts = [];
+router.post('/', async (req,res)=>{
+    let cartNuevo = await manager.addCart();
+    res.send({cartNuevo})
+});
 
-router.get('/', (req,res)=>{
+router.get('/:cid', async (req,res)=>{
+    const id = parseInt(req.params.cid);
+    let cart = await manager.getCart(id);
 
-    res.send({carts});
+    res.send({ cart });
+});
+
+router.post('/:cid/product/:pid', async (req,res) => {
+    try {
+        const idCart = req.params.cid;
+        const idProd = req.params.pid;
+        const resultado = await manager.addProductInCArt(idCart, idProd);
+        
+        res.send(resultado);
+    } catch (error) {
+        res.status(500).send({ error: "error interno" });
+    }
 })
 
-router.post('/', (req,res)=>{
-    const cart = req.body;
-    carts.push(cart);
-    res.send({
-        status: 'Success',
-        carts
-    })
-})
 
 export default router;
